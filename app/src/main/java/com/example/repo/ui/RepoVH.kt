57941692f237228f44110dbdeeb510481ae3919b
Varsha Bhatia <varsha.bhatia@go-jek.com>
2019-12-27 16:45:54 +0530

    Adding the detail view and on tap it should visible and gone

--- app/src/main/java/com/example/repo/ui/RepoVH.kt
+++ app/src/main/java/com/example/repo/ui/RepoVH.kt
@@ -6,13 +6,18 @@ import com.example.repo.extensions.load
 import com.example.repo.githubapi.TrendingRepo
 import kotlinx.android.synthetic.main.rv_repo_item.view.*
 
-class RepoVH ( view: View) : RecyclerView.ViewHolder(view) {
+class RepoVH(view: View) : RecyclerView.ViewHolder(view) {
 
-    fun bind(trendingRepo: TrendingRepo, selectedItem: Int, onRepoClicked : () -> Unit) {
+    fun bind(trendingRepo: TrendingRepo, selectedItem: Int, onRepoClicked: () -> Unit) {
         with(itemView) {
             ivAvatar.load(trendingRepo.avatar!!)
             userName.text = trendingRepo.username
-            repoName.text = trendingRepo.repo.name
+            userUrl.text = trendingRepo.url
+            repoName.text = "Name: "+trendingRepo.repo.name
+            repoDesc.text = "Description: " + trendingRepo.repo.description
+            repoUrl.text = "Url: " + trendingRepo.repo.url
+
+            detailLayout.visibility = if(selectedItem == adapterPosition) View.VISIBLE else View.GONE
 
             setOnClickListener {
                 onRepoClicked()
